dfe03943f17df1897fe801e487e9a3458bb91899
Viet-Trung Luu <viettrungluu@chromium.org>
2016-08-01 16:31:04 -0700

    Add wait set support to mojo.c.
    Change-Id: I7dcc144c7c5086ecd272f3b07e72858777acff47

--- system/mojo.c
+++ system/mojo.c
@@ -4,6 +4,7 @@
 
 #include <assert.h>
 #include <magenta/syscalls.h>
+#include <stddef.h>
 #include <string.h>
 
 #include "mojo/public/c/system/buffer.h"
@@ -11,6 +12,7 @@
 #include "mojo/public/c/system/handle.h"
 #include "mojo/public/c/system/message_pipe.h"
 #include "mojo/public/c/system/wait.h"
+#include "mojo/public/c/system/wait_set.h"
 
 // MojoTimeTicks is in microseconds.
 // mx_time_t is in nanoseconds.
@@ -654,4 +656,137 @@ MojoResult MojoUnmapBuffer(void* buffer) {
 
 // wait_set.h ------------------------------------------------------------------
 
-// TODO(abarth): Not implemented.
+static_assert(sizeof(struct MojoWaitSetResult) == sizeof(mx_wait_set_result_t),
+              "Wait set result types must match");
+static_assert(offsetof(struct MojoWaitSetResult, cookie) ==
+                  offsetof(mx_wait_set_result_t, cookie),
+              "Wait set result types must match");
+static_assert(offsetof(struct MojoWaitSetResult, wait_result) ==
+                  offsetof(mx_wait_set_result_t, wait_result),
+              "Wait set result types must match");
+static_assert(offsetof(struct MojoWaitSetResult, reserved) ==
+                  offsetof(mx_wait_set_result_t, reserved),
+              "Wait set result types must match");
+static_assert(offsetof(struct MojoWaitSetResult, signals_state) ==
+                  offsetof(mx_wait_set_result_t, signals_state),
+              "Wait set result types must match");
+
+// Like |offsetof()|, but includes that data itself.
+// TODO(vtl): This isn't quite right/safe: even if |member_name| is within
+// |EXTENT_OF(struct_type, member_name)|, looking at
+// |struct_instance->member_name| might not be safe.
+#define EXTENT_OF(struct_type, member_name) \
+  (offsetof(struct_type, member_name) + sizeof(((struct_type*)0)->member_name))
+
+MojoResult MojoCreateWaitSet(const struct MojoCreateWaitSetOptions* options,
+                             MojoHandle* handle) {
+  if (options) {
+    if (options->struct_size <
+        EXTENT_OF(struct MojoCreateWaitSetOptions, struct_size))
+      return MOJO_RESULT_INVALID_ARGUMENT;
+    if (options->struct_size >=
+        EXTENT_OF(struct MojoCreateWaitSetOptions, flags)) {
+      // Currently no known flags.
+      if (options->flags)
+        return MOJO_RESULT_UNIMPLEMENTED;
+    }
+  }
+
+  mx_status_t result = mx_wait_set_create();
+  if (result < 0) {
+    switch (result) {
+      case ERR_NO_MEMORY:
+        return MOJO_RESULT_RESOURCE_EXHAUSTED;
+      default:
+        return MOJO_RESULT_UNKNOWN;
+    }
+  }
+  *handle = (MojoHandle)result;
+  return MOJO_RESULT_OK;
+}
+
+MojoResult MojoWaitSetAdd(MojoHandle wait_set_handle,
+                          MojoHandle handle,
+                          MojoHandleSignals signals,
+                          uint64_t cookie,
+                          const struct MojoWaitSetAddOptions* options) {
+  if (options) {
+    if (options->struct_size <
+        EXTENT_OF(struct MojoWaitSetAddOptions, struct_size))
+      return MOJO_RESULT_INVALID_ARGUMENT;
+    if (options->struct_size >=
+        EXTENT_OF(struct MojoWaitSetAddOptions, flags)) {
+      // Currently no known flags.
+      if (options->flags)
+        return MOJO_RESULT_UNIMPLEMENTED;
+    }
+  }
+
+  mx_status_t status = mx_wait_set_add((mx_handle_t)wait_set_handle,
+                                       (mx_handle_t)handle, signals, cookie);
+  switch (status) {
+    case NO_ERROR:
+      return MOJO_RESULT_OK;
+    case ERR_NO_MEMORY:
+      return MOJO_RESULT_RESOURCE_EXHAUSTED;
+    case ERR_BAD_HANDLE:
+      return MOJO_RESULT_INVALID_ARGUMENT;
+    case ERR_INVALID_ARGS:
+      return MOJO_RESULT_INVALID_ARGUMENT;
+    case ERR_ACCESS_DENIED:
+      return MOJO_RESULT_PERMISSION_DENIED;
+    case ERR_NOT_SUPPORTED:
+      // TODO(vtl): |mx_wait_set_add()| returns this is |handle| is not
+      // waitable. There's currently no Mojo/EDK equivalent. Maybe this should
+      // be |MOJO_RESULT_UNIMPLEMENTED| instead?
+      return MOJO_RESULT_INVALID_ARGUMENT;
+    case ERR_ALREADY_EXISTS:
+      return MOJO_RESULT_ALREADY_EXISTS;
+    default:
+      return MOJO_RESULT_UNKNOWN;
+  }
+}
+
+MojoResult MojoWaitSetRemove(MojoHandle wait_set_handle, uint64_t cookie) {
+  mx_status_t status = mx_wait_set_remove((mx_handle_t)wait_set_handle, cookie);
+  switch (status) {
+    case NO_ERROR:
+      return MOJO_RESULT_OK;
+    case ERR_BAD_HANDLE:
+      return MOJO_RESULT_INVALID_ARGUMENT;
+    case ERR_INVALID_ARGS:
+      return MOJO_RESULT_INVALID_ARGUMENT;
+    case ERR_ACCESS_DENIED:
+      return MOJO_RESULT_PERMISSION_DENIED;
+    case ERR_NOT_FOUND:
+      return MOJO_RESULT_NOT_FOUND;
+    default:
+      return MOJO_RESULT_UNKNOWN;
+  }
+}
+
+MojoResult MojoWaitSetWait(MojoHandle wait_set_handle,
+                           MojoDeadline deadline,
+                           uint32_t* num_results,
+                           struct MojoWaitSetResult* results,
+                           uint32_t* max_results) {
+  mx_status_t status = mx_wait_set_wait(
+      (mx_handle_t)wait_set_handle, MojoDeadlineToTime(deadline), num_results,
+      (mx_wait_set_result_t*)results, max_results);
+  switch (status) {
+    case NO_ERROR:
+      return MOJO_RESULT_OK;
+    case ERR_NO_MEMORY:
+      return MOJO_RESULT_RESOURCE_EXHAUSTED;
+    case ERR_BAD_HANDLE:
+      return MOJO_RESULT_INVALID_ARGUMENT;
+    case ERR_INVALID_ARGS:
+      return MOJO_RESULT_INVALID_ARGUMENT;
+    case ERR_ACCESS_DENIED:
+      return MOJO_RESULT_PERMISSION_DENIED;
+    case ERR_TIMED_OUT:
+      return MOJO_RESULT_DEADLINE_EXCEEDED;
+    default:
+      return MOJO_RESULT_UNKNOWN;
+  }
+}
